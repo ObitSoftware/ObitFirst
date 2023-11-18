c988b6a45cae4b27565ca773418c2de9bac26885
Berni Galati <galatib47@gmail.com>
2023-11-18 15:01:05 -0300

    new changes

--- api/routes/prodCompraRoutes.js
+++ api/routes/prodCompraRoutes.js
@@ -3,6 +3,7 @@ const prodcompraRoutes = express.Router();
 
 import {createProdCompra, deleteProdCompra} from '../controllers/prodCompraController.js';
 
+// PRODUCTO DE LA COMPRA 
 
 prodcompraRoutes.post('/', createProdCompra);
 prodcompraRoutes.delete('/:prodCompraId', deleteProdCompra);

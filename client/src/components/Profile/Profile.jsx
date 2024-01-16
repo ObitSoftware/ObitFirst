358dbacb572bd43f07b5867d0c10b163e467895a
Octavio Salas <salasoctavio129@gmail.com>
2024-01-15 21:59:08 -0300

    devs

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -4,6 +4,238 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 import obtenerHoraExacta from '../../functions/actualHour'
 import obtenerFechaActual from '../../functions/actualDate'
+import edit from "../../img/edit.png"
+
+
+const Profile = () => {
+
+    const {clientId} = useParams()
+    const [actualDate, setActualDate] = useState(obtenerFechaActual())
+    const [theHour, setTheHour] = useState(obtenerHoraExacta())
+    const [messageForNote, setMessageForNote] = useState("")
+    const [userData, setUserData] = useState([])
+    const [clientName, setClientName] = useState([])
+    const [allPurchases, setAllPurchases] = useState([])
+    const [allEmails, setAllEmails] = useState([])
+    const [clientPurchases, setClientPurchases] = useState([])
+    const [clientEmails, setClientEmails] = useState([])
+    const [clientAmountSpentOnPurchases, setClientAmountSpentOnPurchases] = useState("")
+    const [wait, setWait] = useState(false)
+    const [userNotes, setUserNotes] = useState([])
+
+    const [showEmails, setShowEmails] = useState(false)
+    const [showActivitie, setShowActivitie] = useState(false)
+    const [showPurchases, setShowPurchases] = useState(false)
+    const [showNotes, setShowNotes] = useState(true)
+
+    useEffect(() => { 
+            axios.get(`http://localhost:3000/clientes/${clientId}`)
+            .then((res) => { 
+             setUserData(res.data)            
+            })
+            .catch((err) => console.log(err))
+
+            axios.get("http://localhost:3000/venta")
+            .then((res) => { 
+             console.log(res.data)
+             setAllPurchases(res.data)
+            })
+           .catch((err) => console.log(err))
+
+            axios.get("http://localhost:3000/email")
+            .then((res) => { 
+             console.log(res.data)
+             setAllEmails(res.data)
+            })
+           .catch((err) => console.log(err))
+
+           axios.get("http://localhost:3000/getNotes")
+           .then((res) => { 
+            console.log(res.data)
+            const dataNotes = res.data
+            setUserNotes(dataNotes.filter((not) => not.addresseeId === clientId))
+           })
+          .catch((err) => console.log(err))
+    }, [])
+
+    useEffect(() => { 
+       setClientName(userData.map((us) => us.nombre))
+       setClientPurchases(allPurchases.filter((pur) => pur.nombreCliente === clientName[0]))
+       setClientAmountSpentOnPurchases(clientPurchases.reduce((acc, el) => acc + el.total, 0))
+       const clientEmail = userData.map((us) => us.email)
+       console.log(clientEmail[0])
+       setClientEmails(allEmails.filter((em) => em.addressee.some((e) => e === clientEmail[0])))
+        setTimeout(() => { 
+           setWait(true)
+        }, 2500)
+      }, [userData, allPurchases, allEmails])
+
+      
+    useEffect(() => { 
+      console.log("NOTAS ---->", userNotes)
+     }, [userNotes])
+
+
+      const createNote = () => { 
+         const noteData = ({
+          date: actualDate,
+          hour: theHour,
+          message: messageForNote,
+          addresseeId: clientId,
+          resolved: false
+         })
+         axios.post("http://localhost:3000/newNote", noteData)
+              .then((res) => console.log(res.data))
+              .catch((err) => console.log(err))
+       }
+
+  
+   const showJustEmails = () => { 
+      setShowActivitie(false)
+      setShowEmails(true)
+      setShowPurchases(false)
+      setShowNotes(false)
+   }
+
+     
+   const showJustNotes = () => { 
+      setShowActivitie(false)
+      setShowEmails(false)
+      setShowPurchases(false)
+      setShowNotes(true)
+   }
+
+     
+   const showJustPurchases = () => { 
+      setShowActivitie(false)
+      setShowEmails(false)
+      setShowPurchases(true)
+      setShowNotes(false)
+   }
+
+   const showJustActivitie = () => { 
+      setShowActivitie(true)
+      setShowEmails(false)
+      setShowPurchases(false)
+      setShowNotes(false)
+   }
+
+
+
+  return (
+    <div>
+       <div className='flex flex-col w-full'>
+           <div className='flex justify-between gap-60 w-full'>
+              <div className='flex justify-start'>
+               {userData.map((us) => ( 
+                  <div className='flex flex-col justify-start items-start'>
+                     <p className='text-xl font-medium text-black'>{us.nombre}</p>       
+                     <p className='text-sm font-medium text-black mt-2'>Cliente</p>
+                  </div>    
+               ))}
+              </div>
+              <div className='flex items-center gap-4'>
+                <button className='rounded-lg bg-blue-400 text-white font-bold text-sm'>Enviar Email</button>
+                <button className='rounded-lg bg-blue-400 text-white font-bold text-sm'>Crear Nota </button>
+                <img src={edit} className='w-6 h-6'/>
+              </div>
+           </div>
+           <div className='flex justify-between gap-60'>
+             <div className='flex flex-col bg-gray-200 w-96'>
+                  <div className='flex flex-col items-start justify-start'>
+                     <h5 className='font-bold text-md'>Estadisticas</h5>
+                     <p className='mt-4'>El usuario esta activo</p>
+                     <p className='mt-1'>El usuario esta activo</p>
+                  </div>
+                  <div className='mt-6 flex flex-col items-start justify-start'>
+                     <h5 className='font-bold text-md'>Informacion Basica</h5>
+                     {userData.map((us) => ( 
+                        <div className='flex flex-col justify-start items-start'>
+                           <p className='text-sm font-medium'>Email: {us.email}</p>
+                           <p className='text-sm font-medium'>Telefono: {us.telefono}</p>
+                           <p className='text-sm font-medium'>Dni: {us.dni}</p>         
+                        </div>    
+                     ))}
+                  </div>
+                  <div className='mt-6 flex flex-col items-start justify-start'>
+                     <h5 className='font-bold text-md'>Ultima Modificacion</h5>
+                     <p  className='text-sm'>Ayer a las 10:15pm</p>
+                  </div>
+
+                  <div className='mt-6 flex flex-col items-start justify-start'>
+                     <h5 className='font-bold text-md'>Detalles</h5>
+                     <p>Monto total gastado por el usuario: {clientAmountSpentOnPurchases}</p>
+                     <p className='mt-2'>Cantidad total de compras realizadas: {clientPurchases.length}</p>
+                  </div>
+             </div>          
+             <div className='flex flex-col'>
+               <div className='flex gap-6  mt-2 w-full'>
+                  <button  className={`rounded-lg items-center text-center h-8 text-xs w-auto ${showActivitie ? 'text-blue-600' : 'text-black' }  font-bold`}  onClick={() => showJustActivitie()}>Ultimos Movimientos </button>
+                  <button className={`rounded-lg items-center text-center h-8 text-xs w-auto ${showNotes ? 'text-blue-600' : 'text-black' }  font-bold`} onClick={() => showJustNotes()}> Notas </button>
+                  <button  className={`rounded-lg items-center text-center h-8 text-xs w-auto ${showEmails ? 'text-blue-600' : 'text-black' }  font-bold`}   onClick={() => showJustEmails()}>Emails </button>
+                  <button  className={`rounded-lg items-center text-center h-8 text-xs w-auto ${showPurchases ? 'text-blue-600' : 'text-black' }  font-bold`}   onClick={() => showJustPurchases()}>Compras </button>
+               </div>
+               <div className='mt-6 bg-gray-300 h-full w-full'>
+               {showNotes === true ? 
+                     <div>
+                        {userNotes.length !== 0 ?
+                           <div className=''>
+                           {userNotes.map((not) => ( 
+                              <div className='flex flex-col '>
+                                 <p>Fecha: {not.date} </p>
+                                 <p>Hora: {not.hour} </p>
+                                 <p>Mensaje: {not.message} </p>
+                              </div>
+                           ))}
+                           </div> : <p>No tienes notas creadas para este usuario</p>}
+                     </div>
+                     :
+                     null                    
+                     }
+
+                     {showPurchases === true ? 
+                     <div>
+                        {clientPurchases.map((cl) => ( 
+                           <div className='mt-6 '>
+                           <p>Producto Comprado: {cl.nombreProducto}</p>
+                           <p>Cantidad Comprada: {cl.cantidad}</p>
+                           <p>Fecha de Compra: {cl.fechaCreacion}</p>
+                           <p>Proveedor Producto: {cl.proveedorProducto}</p>
+                           <p>Total Gastado: {cl.total}</p>
+                           </div>
+                        ))}
+                     </div> : null
+                     }
+
+                     
+                  {showEmails === true ? 
+                     <div>
+                         {clientEmails.map((cl) => ( 
+                           <div className='mt-6 border'>
+                              <p>Fecha: {cl.date}</p>
+                              <p>Hora: {cl.hour}</p>
+                              <p>Mensaje: {cl.message}</p>
+                              <p>Asunto: {cl.title}</p>              
+                           </div>
+                        ))}
+                     </div> : null
+                     }
+               </div>
+             </div>
+           </div>
+       </div>
+    </div>
+  )
+}
+
+export default Profile
+
+/*import React from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import obtenerHoraExacta from '../../functions/actualHour'
+import obtenerFechaActual from '../../functions/actualDate'
 
 const Profile = () => {
 
@@ -168,3 +400,4 @@ const Profile = () => {
 }
 
 export default Profile
+*/
